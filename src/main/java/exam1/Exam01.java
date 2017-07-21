4df1b7e7c30b9aea91c3f81b20a9e68dbe8656df
Krittiya Boonteeranon <krittiya.bo@kasikornbank.com>
2017-07-21 15:58:42 +0700

    แก้ไข Test case ผู้เล่น A และ B ชนะ 1 ครั้งเท่ากัน

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -24,28 +24,53 @@ public class Exam01 {
     }
 
     public String getScore() {
-    	if(winner == firstPerson) {
-    		if(countPlayer1 == 1) {
-    			return "Fifteen-Love";
-    		}else if(countPlayer1 == 2) {
-    			return "Thirty-Love";
-    		}else if(countPlayer1 == 3) {
-    			return "Forty-Love";
-    		}else if(countPlayer1 == 4) {
-    			return "Win for Player A";
-    		}
-    	}else {
-    		if(countPlayer2 == 1) {
-    			return "Love-Fifteen";
-    		}else if(countPlayer2 == 2) {
-    			return "Love-Thirty";
-    		}else if(countPlayer2 == 3) {
-    			return "Love-Forty";
-    		}else if(countPlayer2 == 4) {
-    			return "Win for Player B";
-    		}
+//    	if(winner == firstPerson) {
+//    		if(countPlayer1 == 1) {
+//    			return "Fifteen-Love";
+//    		}else if(countPlayer1 == 2) {
+//    			return "Thirty-Love";
+//    		}else if(countPlayer1 == 3) {
+//    			return "Forty-Love";
+//    		}else if(countPlayer1 == 4) {
+//    			return "Win for Player A";
+//    		}
+//    	}else {
+//    		if(countPlayer2 == 1) {
+//    			return "Love-Fifteen";
+//    		}else if(countPlayer2 == 2) {
+//    			return "Love-Thirty";
+//    		}else if(countPlayer2 == 3) {
+//    			return "Love-Forty";
+//    		}else if(countPlayer2 == 4) {
+//    			return "Win for Player B";
+//    		}
+//    	}
+    	
+    	if(countPlayer1 == 1) {
+			return "Fifteen-Love";
+		}else if(countPlayer1 == 2) {
+			return "Thirty-Love";
+		}else if(countPlayer1 == 3) {
+			return "Forty-Love";
+		}else if(countPlayer1 == 4) {
+			return "Win for Player A";
+		}
+    	
+    	if(countPlayer2 == 1) {
+			return "Love-Fifteen";
+		}else if(countPlayer2 == 2) {
+			return "Love-Thirty";
+		}else if(countPlayer2 == 3) {
+			return "Love-Forty";
+		}else if(countPlayer2 == 4) {
+			return "Win for Player B";
+		}
+    	
+    	if(countPlayer1 == countPlayer2 && countPlayer1 == 1) {
+    		return "Love-All";
     	}
-        return "Love-All";
+    	return "Love-All";
+        
     }
 
 }
