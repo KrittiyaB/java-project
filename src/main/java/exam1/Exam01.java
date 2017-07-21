70ebca39c3bd7c16a943f7151c7171bdd98a4d45
Krittiya Boonteeranon <krittiya.bo@kasikornbank.com>
2017-07-21 15:27:09 +0700

    แก้ไข test case 1 และ 2

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -1,19 +1,28 @@
 package exam1;
 
 public class Exam01 {
+	private String firstPerson;
+	private String secondPerson;
+	private String winner;
 
     public Exam01(String firstPerson, String secondPerson) {
+    	this.firstPerson = firstPerson;
+    	this.secondPerson = secondPerson;
     }
 
 
     public void playerAWin() {
+    	winner = firstPerson;
     }
 
     public void playerBWin() {
     }
 
     public String getScore() {
-        return null;
+    	if(winner == firstPerson) {
+    		return "Fifteen-Love";
+    	}
+        return "Love-All";
     }
 
 }
