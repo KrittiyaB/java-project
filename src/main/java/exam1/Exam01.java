c3a7c11d731ed61f9b7a08e6b67d52303b47c294
GIK <GIK@GIK>
2017-07-25 21:06:08 +0700

    Refactor code (use firstPerson and secondPerson)

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -6,72 +6,67 @@ public class Exam01 {
 	private int countPlayerA = 0;
 	private int countPlayerB = 0;
 
-    public Exam01(String firstPerson, String secondPerson) {
-    	this.firstPerson = firstPerson;
-    	this.secondPerson = secondPerson;
-    }
+	public Exam01(String firstPerson, String secondPerson) {
+		this.firstPerson = firstPerson;
+		this.secondPerson = secondPerson;
+	}
 
-    public void playerAWin() {
-    	countPlayerA++;
-    }
+	public void playerAWin() {
+		countPlayerA++;
+	}
 
-    public void playerBWin() {
-    	countPlayerB++;
-    }
+	public void playerBWin() {
+		countPlayerB++;
+	}
+	
+	public String getScore() {
+		String resultA = "";
+		String resultB = "";
+		String resultFinal = "";
 
-    public String getScore() {
-    	String resultA = "";
-    	String resultB = "";
-    	String resultFinal = "";
-    	
-    	if(countPlayerA == 0 && countPlayerB == 0) {
-    		resultFinal = "Love-All";
-    	}
-    	
-    	if(countPlayerA == 0) {
-    		resultA = "Love";
-    	}else if(countPlayerA == 1) {
-    		resultA = "Fifteen";
-    	}else if(countPlayerA == 2) {
-    		resultA = "Thirty";
-    	}else if(countPlayerA == 3) {
-    		resultA = "Forty";
-    	}
-    	
-    	if(countPlayerB == 0) {
-    		resultB = "Love";
-    	}else if(countPlayerB == 1) {
-    		resultB = "Fifteen";
-    	}else if(countPlayerB == 2) {
-    		resultB = "Thirty";
-    	}else if(countPlayerB == 3) {
-    		resultB = "Forty";
-    	}
-    	
-    	if(countPlayerA == countPlayerB && countPlayerA >= 3 && countPlayerA <= 4 ) {
-    		resultFinal = "Deuce";	
-    	}else if(countPlayerA == countPlayerB) {
-    		resultFinal = resultA + "-" + "All";
-    	}else if(countPlayerA <= 3 && countPlayerB <= 3) {
-    		resultFinal = resultA + "-" + resultB;
-    	}else if(countPlayerA >= 4 || countPlayerB >=4) {
-    		if(countPlayerA - countPlayerB >= 2) {
-    			resultFinal = "Win for Player A";
-    		}else if(countPlayerB - countPlayerA >= 2) {
-    			resultFinal = "Win for Player B";
-    		}else if(countPlayerA - countPlayerB == 1) {
-    			resultFinal = "Advantage Player A";
-    		}else if(countPlayerB - countPlayerA == 1) {
-    			resultFinal = "Advantage Player B";
-    		}
-    	}
-    	
-    	
-    	
-    	
-    	
-    	
-    	return resultFinal;
-    }
+		if (countPlayerA == 0 && countPlayerB == 0) {
+			resultFinal = "Love-All";
+		}
+
+		if (countPlayerA == 0) {
+			resultA = "Love";
+		} else if (countPlayerA == 1) {
+			resultA = "Fifteen";
+		} else if (countPlayerA == 2) {
+			resultA = "Thirty";
+		} else if (countPlayerA == 3) {
+			resultA = "Forty";
+		}
+
+		if (countPlayerB == 0) {
+			resultB = "Love";
+		} else if (countPlayerB == 1) {
+			resultB = "Fifteen";
+		} else if (countPlayerB == 2) {
+			resultB = "Thirty";
+		} else if (countPlayerB == 3) {
+			resultB = "Forty";
+		}
+
+		if (countPlayerA == countPlayerB && countPlayerA >= 3 && countPlayerA <= 4) {
+			resultFinal = "Deuce";
+		} else if (countPlayerA == countPlayerB) {
+			resultFinal = resultA + "-" + "All";
+		} else if (countPlayerA <= 3 && countPlayerB <= 3) {
+			resultFinal = resultA + "-" + resultB;
+		} else if (countPlayerA >= 4 || countPlayerB >= 4) {
+			if (countPlayerA - countPlayerB >= 2) {
+				resultFinal = "Win for " + firstPerson;
+			} else if (countPlayerB - countPlayerA >= 2) {
+				resultFinal = "Win for " + secondPerson;
+			} else if (countPlayerA - countPlayerB == 1) {
+				resultFinal = "Advantage " + firstPerson;
+			} else if (countPlayerB - countPlayerA == 1) {
+				resultFinal = "Advantage " + secondPerson;
+			}
+		}
+
+		return resultFinal;
+	}
 
 }
